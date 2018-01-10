e1045f71c8044641220f4581bce90e10225aaf25
Samantha Neilen <33734383+SamanthaNeilen@users.noreply.github.com>
2018-01-10 17:53:56 +0100

    Added Selenium web test for comparison purposes

--- CodedUITestTestApplication.Tests/WebTests.cs
+++ CodedUITestTestApplication.Tests/WebTests.cs
@@ -2,8 +2,13 @@
 using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
 using System;
 using System.Configuration;
+using System.Linq;
 
 namespace CodedUITestTestApplication.Tests
 {
@@ -50,10 +55,60 @@ namespace CodedUITestTestApplication.Tests
             var emailAddessValidationMessage = new HtmlSpan(browser);
             emailAddessValidationMessage.SearchProperties.Add(HtmlControl.PropertyNames.Class, "field-validation-error");
             emailAddessValidationMessage.SearchProperties.Add(HtmlControl.PropertyNames.InnerText, expectedEmailvalidationMessage, PropertyExpressionOperator.Contains);
-            Assert.IsTrue(emailAddessValidationMessage.TryFind());
 
-            var expectedEmailMessage = string.Format(Messages.FieldRequired, Labels.EmailAddress);
-            Assert.IsTrue(emailAddessValidationMessage.InnerText.Equals(expectedEmailMessage));
+            Assert.IsTrue(emailAddessValidationMessage.TryFind());
+            Assert.IsTrue(emailAddessValidationMessage.InnerText.Equals(expectedEmailvalidationMessage));
+        }
+
+        [TestMethod, DataSource("Browsers")]
+        public void WebTest_Selenium_TestForm()
+        {
+            var browserIdentifier = "Chrome";
+            if (TestContext.DataRow != null)
+            {
+                browserIdentifier = TestContext.DataRow[0].ToString();
+            }
+
+            var driver = StartWebdriver(browserIdentifier);
+
+            driver.Navigate().GoToUrl(new Uri(StartUrl));
+
+            var nameTextBox = driver.FindElement(By.Id("Name"));
+            nameTextBox.SendKeys("Some Name");
+
+            var submitButton = driver.FindElement(By.TagName("button"));
+            submitButton.Click();
+
+            var header = driver.FindElement(By.Id("addCustomerHeader"));
+            Assert.IsTrue(header.Displayed);
+
+            Assert.AreEqual(0, driver.FindElements(By.Id("Name-error")).Count);
+
+            var expectedEmailvalidationMessage = string.Format(Messages.FieldRequired, Labels.EmailAddress);
+            var validationMessages = driver.FindElements(By.CssSelector(".field-validation-error"));
+            Assert.IsTrue(validationMessages.Any(element => element.Displayed && ChildSpanContainsExpectedText(element, expectedEmailvalidationMessage)));
+
+            driver.Close();
+        }
+
+        private static bool ChildSpanContainsExpectedText(IWebElement element, string expectedEmailvalidationMessage)
+        {
+            return element.FindElement(By.TagName("span")).Text.Equals(expectedEmailvalidationMessage);
+        }
+
+        private IWebDriver StartWebdriver(string browserIdentifier)
+        {
+            switch (browserIdentifier)
+            {
+                case "IE":
+                    return new InternetExplorerDriver();
+                case "Firefox":
+                    return new FirefoxDriver();
+                case "Chrome":
+                    return new ChromeDriver();
+                default:
+                    throw new NotSupportedException();
+            }
         }
     }
 }
